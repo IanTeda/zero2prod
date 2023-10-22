26b944c2c4921050dfccfaad124a11e44741073a
Ian Teda <ian@teda.id.au>
2023-10-22 11:25:52 +1100

    Chapter 3

---
+++ tests/health_check.rs
@@ -0,0 +1,34 @@
+use std::net::TcpListener;
+use zero2prod::run;
+
+// Start (spawn) the server and return the port used
+// Spawned app cleans up the instance and port for us
+fn spawn_app() -> String {
+    // With port 0 the OS selects the port for us
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
+}
+
+#[actix_rt::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        // Use the returned application address
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
